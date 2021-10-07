cd915e0a7c56d959328d9a8169ec55702be35232
Casper van Engelenburg <40263235+caspervanengelenburg@users.noreply.github.com>
2021-10-07 12:12:32 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,4 +1,11 @@
+from pytest import approx
 temperature = 20
 
 def print_temperature():
     print(temperature)
+
+def add(a, b):
+    return a + b
+
+def test_add():
+    assert add(2.0, 3.0) = approx(5.0)
